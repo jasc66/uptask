06b33b32a9a435f126a2d14ba6426a63e3a32821
Alonso Salguero <jalonsc66@gmail.com>
2023-06-29 07:49:02 -0600

    Se empieza agregar el controllers de proyectos

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,11 +1,23 @@
 import express from "express";
-import { registrar, autenticar } from "../contollers/usuarioController.js";
-
+import {
+    registrar,
+    autenticar,
+    confirmar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword,
+    perfil
+} from "../contollers/usuarioController.js";
+import checkAuth from "../middleware/checkAuth.js";
 const router = express.Router();
 
 //Autenticación, registro y confirmación de Usuarios
 router.post('/', registrar); //Crear un nuevo usuario 
-router.post('/login', autenticar)
+router.post('/login', autenticar);
+router.get('/confirmar/:token', confirmar);
+router.post('/olvide-password', olvidePassword);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+router.get('/perfil', checkAuth, perfil);
 
 
 export default router
