5c3ce0ea17ed65f0e408860246f10fa445c26b43
Alonso Salguero <33616594+jasc66@users.noreply.github.com>
2026-05-17 18:10:10 -0600

    Fix: descomentar log de MongoDB Connected en db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const conectarDB = async () => {
 	try {
 		const connection = await mongoose.connect(process.env.MONGO_URI);
 		const url = `${connection.connection.host}:${connection.connection.port}`;
-		//console.log(`MongoDB Conectado en: ${url} `);
+		        console.log(`MongoDB Conectado en: ${url} `);
 	} catch (error) {
 		console.log(`error: ${error.message}`);
 		process.exit(1);
